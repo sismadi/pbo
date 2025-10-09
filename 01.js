7992523ab384b7a78026e4df13a27358a4e07a54
hildahiu14 <hildamayanti14@gmail.com>
2025-10-09 23:10:57 +0700

    Update 01.js

--- 01.js
+++ 01.js
@@ -1,11 +1,11 @@
- class Sepatu {
+class Sepatu {
  constructor(merk, warna) {
- this.merk = merk;
- this.warna = warna;
+  this.merk = merk;
+  this.warna = warna;
  }
  info() {
- return `Sepatu ini adalah ${this.merk} berwarna ${this.warna}.`;
+  return `Sepatu ini adalah ${this.merk} berwarna ${this.warna}.`;
  }
- }
- const brodo = new Sepatu("sepatu Brodo", "abu-abu");
- console.log(brodo.info())
+}
+const brodo = new Sepatu("sepatu Brodo", "abu-abu");
+console.log(brodo.info())
