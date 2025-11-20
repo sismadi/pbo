1baa12f1fb96eba2ca7702a510a90d9799f8470c
hildahiu14 <hildamayanti14@gmail.com>
2025-11-20 19:33:42 +0700

    Create 11.js

---
+++ 11.js
@@ -0,0 +1,9 @@
+ let mahasiswa = new Map();
+ mahasiswa.set("001", "Andi");
+ mahasiswa.set("002", "Budi");
+ mahasiswa.set("003", "Vera");
+ mahasiswa.set("004", "Putra");
+ console.log(mahasiswa.get("001")); // Output: Andi
+ console.log(mahasiswa.get("002")); // Output: Budi
+ console.log(mahasiswa.get("003")); // Output: Vera
+ console.log(mahasiswa.get("004")); // Output: Putra
