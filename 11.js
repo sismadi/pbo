600fd59bfeed7fd3eb98c1daf62f4e85e1e13ec7
hildahiu14 <hildamayanti14@gmail.com>
2025-11-20 19:46:24 +0700

    Update 11.js

--- 11.js
+++ 11.js
@@ -7,3 +7,9 @@
  console.log(mahasiswa.get("002")); // Output: Budi
  console.log(mahasiswa.get("003")); // Output: Vera
  console.log(mahasiswa.get("004")); // Output: Putra
+
+let mahasiswi = ["Andi", "Budi", "Citra", "Dewi"];
+ console.log(mahasiswi[2]); // Output: Andi
+ 
+let angka = new Set([1, 2, 3, 3, 4, 5, 5]);
+console.log(angka); // Output: {1, 2, 3, 4, 5}
